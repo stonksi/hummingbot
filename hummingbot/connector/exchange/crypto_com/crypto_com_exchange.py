5a827a77f79384a8555381484886e46b5fd6396b
stonksi <nothanks@gmail.com>
2021-03-11 14:38:50 +0100

    Revert "Fee fix v3"
    This reverts commit aab8d2f98f56e95d686f99402c8cae490dd85c53.

--- hummingbot/connector/exchange/crypto_com/crypto_com_exchange.py
+++ hummingbot/connector/exchange/crypto_com/crypto_com_exchange.py
@@ -661,9 +661,9 @@ class CryptoComExchange(ExchangeBase):
             return
 
         if tracked_order.order_type is OrderType.LIMIT_MAKER:
-            feepercent = Decimal(0.036)
+            feepercent = "0.036"
         else:
-            feepercent = Decimal(0.06)
+            feepercent = "0.06"
 
         self.trigger_event(
             MarketEvent.OrderFilled,
@@ -675,7 +675,7 @@ class CryptoComExchange(ExchangeBase):
                 tracked_order.order_type,
                 Decimal(str(trade_msg["traded_price"])),
                 Decimal(str(trade_msg["traded_quantity"])),
-                TradeFee(Decimal(0.0), ["CRO", feepercent]),
+                TradeFee(0.0, ["CRO", feepercent]),
                 exchange_trade_id=trade_msg["order_id"]
             )
         )
