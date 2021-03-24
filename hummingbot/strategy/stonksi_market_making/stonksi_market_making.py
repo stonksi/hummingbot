e6acb65cd62ac9083f49a4570263685bae2eec7e
stonksi <reifureifu@gmail.com>
2021-03-24 10:42:25 +0100

    Fixes to status window in Stonksi_MM

--- hummingbot/strategy/stonksi_market_making/stonksi_market_making.py
+++ hummingbot/strategy/stonksi_market_making/stonksi_market_making.py
@@ -181,7 +181,7 @@ class StonksiMarketMakingStrategy(StrategyPyBase):
 
     def market_status_df(self) -> pd.DataFrame:
         data = []
-        columns = ["Market", "Mid price", "Best bid", "Best ask"]#, "Volatility"]
+        columns = ["Market", "Mid price", "Best bid", "%", "Best ask", "%"]#, "Volatility"]
         for market, market_info in self._market_infos.items():
             mid_price = market_info.get_mid_price()
             best_bid = self._exchange.get_price(market, False)
@@ -191,8 +191,10 @@ class StonksiMarketMakingStrategy(StrategyPyBase):
             data.append([
                 market,
                 float(mid_price),
-                f"{best_bid_pct:.2%}",
-                f"{best_ask_pct:.2%}",
+                float(best_bid),
+                f"{best_bid_pct:.3%}",
+                float(best_ask)                
+                f"{best_ask_pct:.3%}"
                 #"" if self._volatility[market].is_nan() else f"{self._volatility[market]:.2%}",
             ])
         df = pd.DataFrame(data=data, columns=columns).replace(np.nan, '', regex=True)
