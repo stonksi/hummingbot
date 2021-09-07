74f8ec1383ad38a0421ed931629cc497cd30795c
stonksi <reifureifu@gmail.com>
2021-09-07 07:39:24 +0200

    Added limit to cancel_all_orders on crypto_com

--- hummingbot/connector/exchange/crypto_com/crypto_com_constants.py
+++ hummingbot/connector/exchange/crypto_com/crypto_com_constants.py
@@ -37,6 +37,7 @@ RATE_LIMITS = [
     RateLimit(limit_id=GET_TRADING_RULES_PATH_URL, limit=100, time_interval=1),
     RateLimit(limit_id=CREATE_ORDER_PATH_URL, limit=15, time_interval=0.1),
     RateLimit(limit_id=CANCEL_ORDER_PATH_URL, limit=15, time_interval=0.1),
+    RateLimit(limit_id=CANCEL_ALL_ORDERS_PATH_URL, limit=15, time_interval=0.1),
     RateLimit(limit_id=GET_ACCOUNT_SUMMARY_PATH_URL, limit=3, time_interval=0.1),
     RateLimit(limit_id=GET_ORDER_DETAIL_PATH_URL, limit=30, time_interval=0.1),
     RateLimit(limit_id=GET_OPEN_ORDERS_PATH_URL, limit=3, time_interval=0.1),
