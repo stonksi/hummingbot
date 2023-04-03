79adef3ab0758ebf06f2f27b29c9cc89c18e8db7
stonksi <43435291+stonksi@users.noreply.github.com>
2023-04-03 07:46:31 +0200

    c_quantize_order_price // -> /

--- hummingbot/connector/connector_base.pyx
+++ hummingbot/connector/connector_base.pyx
@@ -472,7 +472,7 @@ cdef class ConnectorBase(NetworkIterator):
         if price.is_nan():
             return price
         price_quantum = self.c_get_order_price_quantum(trading_pair, price)
-        return (price // price_quantum) * price_quantum
+        return (price / price_quantum) * price_quantum
 
     def quantize_order_price(self, trading_pair: str, price: Decimal) -> Decimal:
         """
