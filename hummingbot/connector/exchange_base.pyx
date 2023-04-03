57d43cedd4eba762f593b603aa04a7d90f0be7e5
stonksi <43435291+stonksi@users.noreply.github.com>
2023-04-03 08:05:01 +0200

    Decimal(str)

--- hummingbot/connector/exchange_base.pyx
+++ hummingbot/connector/exchange_base.pyx
@@ -196,7 +196,7 @@ cdef class ExchangeBase(ConnectorBase):
             OrderBook order_book = self.c_get_order_book(trading_pair)
             OrderBookQueryResult result = order_book.c_get_price_for_quote_volume(is_buy, float(volume))
             object query_volume = Decimal(str(result.query_volume))
-            object result_price = self.c_quantize_order_price(trading_pair, Decimal(result.result_price))
+            object result_price = self.c_quantize_order_price(trading_pair, Decimal(str(result.result_price)))
             object result_volume = Decimal(str(result.result_volume))
         return ClientOrderBookQueryResult(s_decimal_NaN,
                                           query_volume,
@@ -207,9 +207,9 @@ cdef class ExchangeBase(ConnectorBase):
         cdef:
             OrderBook order_book = self.c_get_order_book(trading_pair)
             OrderBookQueryResult result = order_book.c_get_price_for_volume(is_buy, float(volume))
-            object query_volume = self.c_quantize_order_amount(trading_pair, Decimal(result.query_volume))
-            object result_price = self.c_quantize_order_price(trading_pair, result.result_price)
-            object result_volume = self.c_quantize_order_amount(trading_pair, Decimal(result.result_volume))
+            object query_volume = self.c_quantize_order_amount(trading_pair, Decimal(str(result.query_volume)))
+            object result_price = self.c_quantize_order_price(trading_pair, Decimal(str(result.result_price)))
+            object result_volume = self.c_quantize_order_amount(trading_pair, Decimal(str(result.result_volume)))
         ###### TEMP
         self.notify_hb_app_with_timestamp(f"result.result_price = {result.result_price}")  
         self.notify_hb_app_with_timestamp(f"Decimal(result.result_price) = {Decimal(result.result_price)}")   
